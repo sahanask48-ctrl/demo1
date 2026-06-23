d224e19bf40a7b8e25aef9c2d87b61a01fe796aa
sahana <sahana@gmail.com>
2026-06-23 13:28:53 +0530

    Modified master.java

---
+++ demo.java
@@ -0,0 +1,6 @@
+class Main{
+  public static void main(String  args[]){
+    System.out.prinltn("Hello, This is sahana");
+    System.out.prinltn("Currently intern at dhee coding lab");
+  }
+}
--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 class Main{
   public static void main(String  args[]){
     System.out.prinltn("Hello world");
+    System.out.prinltn("Dhee coding lab");
   }
 }
